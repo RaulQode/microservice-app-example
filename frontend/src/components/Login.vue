47357c17f8e62083f14a87c0c84caf9fbcd21196
Geoffreyopv <geoffreypv00@gmail.com>
2025-09-28 23:07:55 -0500

    feat(frontend): Update login page title

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -7,6 +7,7 @@
                 <div class='row'>
                     <div class='col-md-3'></div>
                     <div class='col-md-6'>
+                        <h3>Bienvenido al Taller de DevOps</h3>
                         <h2>Please Login</h2>
                         <hr>
                     </div>
